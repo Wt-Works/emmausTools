23a2c4345cdd81e050d81f3e3ba98ed2cde44041
Matthew Sherborne <msherborne@gmail.com>
2014-09-30 02:36:12 +1000

    Now modifies the file and allows you to download it

--- FileUpdaterTools.cpp
+++ FileUpdaterTools.cpp
@@ -1,22 +1,47 @@
 #include "FileUpdaterTools.hpp"
 
 #include <string>
+#include <fstream>
+#include <vector>
 
 #include <Wt/WText>
-#include <Wt/WFileUpload>
 #include <Wt/WPushButton>
+#include <Wt/WContainerWidget>
+#include <Wt/WMemoryResource>
 
 FileUpdaterTools::FileUpdaterTools(Wt::WContainerWidget* parent) : Wt::WContainerWidget(parent)
 {
      new Wt::WText("Please upload the file", this);
-     uploader = new Wt::WFileUpload(this);
+     body = new Wt::WContainerWidget(this);
+     uploader = new Wt::WFileUpload(body);
      uploader->uploaded().connect(this, &FileUpdaterTools::fileUploaded);
-     Wt::WPushButton* uploadBtn = new Wt::WPushButton("Upload", this);
+     Wt::WPushButton* uploadBtn = new Wt::WPushButton("Upload", body);
      uploadBtn->clicked().connect(uploader, &Wt::WFileUpload::upload);
 }
 
 void FileUpdaterTools::fileUploaded() {
-    std::string fn = uploader->spoolFileName();
-    delete uploader;
-    new Wt::WText(std::string("Your file was uploaded to: ") + fn, this);
+    // Open the file
+    std::ifstream file(uploader->spoolFileName().c_str());
+    // We'll copy it to this block of memory
+    std::vector<unsigned char> data;
+    // Using this iterator
+    std::back_insert_iterator<std::vector<unsigned char> > out(data);
+    // process the file
+    const std::string ending = " store 33\r\n";
+    while (file.good()) {
+        std::string line;
+        std::getline(file, line);
+        if (line.empty()) {
+            *(out++) = '\r';
+            *(out++) = '\n';
+        } else {
+            std::copy(line.begin(), line.end(), out);
+            std::copy(ending.begin(), ending.end(), out);
+        }
+    }
+    // Turn the fixed file into a resource
+    Wt::WMemoryResource* resource = new Wt::WMemoryResource("text/plain", data, this);
+    resource->suggestFileName("modified-file.txt", Wt::WResource::Attachment);
+    body->clear(); // This will delete the upload box, and the upload button
+    new Wt::WAnchor(resource, "Download fixed version", body);
 }
--- FileUpdaterTools.hpp
+++ FileUpdaterTools.hpp
@@ -1,14 +1,12 @@
 #pragma once
 
 #include <Wt/WContainerWidget>
+#include <Wt/WFileUpload>
 #include <Wt/WString>
-
-namespace Wt {
-class WFileUpload;
-}
 
 class FileUpdaterTools : public Wt::WContainerWidget {
 private:
+    Wt::WContainerWidget* body;
     Wt::WFileUpload* uploader;
     void fileUploaded();
 public:
